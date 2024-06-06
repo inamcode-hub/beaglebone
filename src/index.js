1d85cb0816f4474c54378eaaaccbea856e2da774
Inam Ul Rehman <inam@dryermaster.com>
2024-06-06 15:44:56 -0400

    chore: Update server response message to display latest version

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) =>
-  res.send('<h1>Hello World!</h1><p>Running version v.1.1</p>')
+  res.send('<h1>Hello World!</h1><p>Running latest version</p>')
 );
 
 app.listen(port, () => {

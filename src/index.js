600e25121e9c745d7419919c1ef74eeb48d28648
Inam Ul Rehman <inam@dryermaster.com>
2024-06-11 10:14:16 -0400

    chore: Update Docker configuration and deployment scripts and add version file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
+// Read the version from the VERSION file
+const versionPath = '/usr/src/app/VERSION';
+let version = 'unknown';
+if (fs.existsSync(versionPath)) {
+  version = fs.readFileSync(versionPath, 'utf8').trim();
+}
+
 app.get('/', (req, res) =>
-  res.send('<h1>Hello World!</h1><p>Running latest version 1.9</p>')
+  res.send(`<h1>Hello World!</h1><p>Running latest version ${version}</p>`)
 );
 
 app.listen(port, () => {

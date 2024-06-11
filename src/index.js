51ee263a06d6b5e0d1f2c6486524478a887bf8a0
Inam Ul Rehman <inam@dryermaster.com>
2024-06-11 09:05:21 -0400

    chore: testing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) =>
-  res.send('<h1>Hello World!</h1><p>Running latest version</p>')
+  res.send('<h1>Hello World!</h1><p>Running latest version 1.9</p>')
 );
 
 app.listen(port, () => {

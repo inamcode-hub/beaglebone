63346aff3e6617d918fbac8d5dc6a50f4e4a765b
Inam Ul Rehman <inam@dryermaster.com>
2024-06-11 10:55:37 -0400

    chore: Update index.js to include new repository information

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ if (fs.existsSync(versionPath)) {
 }
 
 app.get('/', (req, res) =>
-  res.send(`<h1>Hello World!</h1><p>Running latest version ${version}</p>`)
+  res.send(
+    `<h1>Hello World!</h1><p>Running latest version ${version}.New Repo.</p>`
+  )
 );
 
 app.listen(port, () => {

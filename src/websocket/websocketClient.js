a6cfce1b977e8fd7acc4359373efb546897aaaeb
Inam Ul Rehman <inam@dryermaster.com>
2024-06-20 15:34:16 -0400

    chore: Refactor WebSocket client to handle reconnection

--- src/websocket/websocketClient.js
+++ src/websocket/websocketClient.js
@@ -2,28 +2,38 @@ const WebSocket = require('ws');
 const logger = require('../config/logger');
 const { handleMessage } = require('../controllers/websocketController');
 
+const RECONNECT_INTERVAL = 5000; // Time in milliseconds to wait before attempting to reconnect
+
 function initWebSocket() {
-  const ws = new WebSocket(process.env.WEBSOCKET_SERVER_URL);
+  let ws;
 
-  console.log(
-    `WebSocket client connecting to ${process.env.WEBSOCKET_SERVER_URL}`
-  );
+  function connect() {
+    ws = new WebSocket(process.env.WEBSOCKET_SERVER_URL);
 
-  ws.on('open', () => {
-    logger.info('WebSocket connection established');
-  });
+    console.log(
+      `WebSocket client connecting to ${process.env.WEBSOCKET_SERVER_URL}`
+    );
 
-  ws.on('message', (message) => {
-    handleMessage(ws, message);
-  });
+    ws.on('open', () => {
+      logger.info('WebSocket connection established');
+    });
 
-  ws.on('close', () => {
-    logger.info('WebSocket connection closed');
-  });
+    ws.on('message', (message) => {
+      handleMessage(ws, message);
+    });
 
-  ws.on('error', (error) => {
-    logger.error(`WebSocket error: ${error.message}`);
-  });
+    ws.on('close', () => {
+      logger.info('WebSocket connection closed. Attempting to reconnect...');
+      setTimeout(connect, RECONNECT_INTERVAL);
+    });
+
+    ws.on('error', (error) => {
+      logger.error(`WebSocket error: ${error.message}`);
+      ws.close(); // Close the connection and trigger the reconnect logic
+    });
+  }
+
+  connect(); // Initiate the first connection
 
   return ws;
 }

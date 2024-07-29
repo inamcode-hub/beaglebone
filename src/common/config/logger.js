350e3dec367fe64f8fc2ab20129c7317482dc863
Inam Ul Rehman <inam@dryermaster.com>
2024-07-29 09:12:47 -0400

    refactor: Update logger configuration to set default log level to debug

--- src/common/config/logger.js
+++ src/common/config/logger.js
@@ -1,17 +1,19 @@
 import { createLogger, format, transports } from 'winston';
 const { combine, timestamp, printf, colorize } = format;
 
+// Define custom log format
 const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`;
 });
 
+// Create a logger
 const logger = createLogger({
-  level: 'info',
+  level: 'debug', // Set default level to debug to log all messages
   format: combine(timestamp(), logFormat),
   transports: [
     new transports.Console({
       format: combine(
-        colorize(), // Add colors only for console output
+        colorize(), // Use default colors for all levels
         timestamp(),
         logFormat
       ),

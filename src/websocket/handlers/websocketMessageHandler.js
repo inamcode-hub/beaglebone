90dc1efbb0d672c153b576a5dc2cbe1afe5c942a
Inam Ul Rehman <inam@dryermaster.com>
2024-08-07 10:19:22 -0400

    feat: Add reboot functionality to WebSocket server

--- src/websocket/handlers/websocketMessageHandler.js
+++ src/websocket/handlers/websocketMessageHandler.js
@@ -4,7 +4,9 @@ import {
 } from '../../modbus/controllers/modbusController.js';
 import { sendMessage, handleError } from '../utils/websocketUtils.js';
 import MESSAGE_TYPES from '../constants/messageTypes.js';
-
+import { exec } from 'child_process';
+import e from 'express';
+import logger from '../../common/config/logger.js';
 async function handleRequestSensorData(ws) {
   try {
     const data = await getModbusData();
@@ -31,6 +33,56 @@ async function handleUpdateDeviceSettings(ws, message) {
   }
 }
 
+async function rebootDevice(ws) {
+  try {
+    const data = await getModbusData();
+    const serialNumber = data.find(
+      (item) => item.tagName === 'systemSerialNumberWriteOnly'
+    ).value;
+    // Sending successful reboot message
+    sendMessage(ws, MESSAGE_TYPES.REBOOT_DEVICE_ACK, {
+      serialNumber,
+      success: true,
+      message: 'Device reboot initiated successfully.',
+    });
+    logger.info(`Device reboot initiated successfully: ${serialNumber}`);
+    // Execute the reboot script
+    exec(
+      'sudo /usr/local/bin/scripts/reboot-system.sh',
+      (error, stdout, stderr) => {
+        if (error) {
+          logger.error(`Reboot error: ${error.message}`);
+          sendMessage(ws, MESSAGE_TYPES.REBOOT_DEVICE_ACK, {
+            serialNumber,
+            success: false,
+            error: 'Failed to reboot the device.',
+            errorMessage: error.message,
+          });
+          return;
+        }
+        if (stderr) {
+          logger.error(`Reboot stderr: ${stderr}`);
+          sendMessage(ws, MESSAGE_TYPES.REBOOT_DEVICE_ACK, {
+            serialNumber,
+            success: false,
+            error: 'Failed to reboot the device.',
+            errorMessage: stderr,
+          });
+          return;
+        }
+      }
+    );
+  } catch (error) {
+    sendMessage(ws, MESSAGE_TYPES.REBOOT_DEVICE_ACK, {
+      serialNumber,
+      success: false,
+      error: 'Failed to reboot the device.',
+      errorMessage: error.message,
+    });
+    handleError(ws, error);
+  }
+}
+
 export async function handleMessage(ws, message) {
   try {
     const parsedMessage = JSON.parse(message);
@@ -41,6 +93,9 @@ export async function handleMessage(ws, message) {
         break;
       case MESSAGE_TYPES.UPDATE_DEVICE_SETTINGS:
         await handleUpdateDeviceSettings(ws, parsedMessage);
+        break;
+      case MESSAGE_TYPES.REBOOT_DEVICE:
+        await rebootDevice(ws);
         break;
       default:
         console.warn(`Unknown message type: ${parsedMessage.type}`);
